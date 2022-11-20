799ca5ee211cff8e783f85d35dc8852489a7c4cc
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-11-20 14:04:39 -0300

    feat: Enviando requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,18 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (len(nums) <= 1):
+        return False
+
+    nums.sort()
+    leng = len(nums)
+
+    for i in range(0, leng):
+        if (
+            not isinstance(nums[i], int)
+            or nums[i] < 0
+        ):
+            return False
+        elif i + 1 != leng:
+            if nums[i] == nums[i + 1]:
+                return nums[i]
+
+    return False
