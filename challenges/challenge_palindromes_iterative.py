2f9e55cb54901598243c599d00ab15387cf0feb7
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-11-20 14:14:40 -0300

    feat: Enviando requisito 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+
+    char_set = list(word)
+    palindrome = "".join(reversed(char_set))
+
+    if word == palindrome:
+        return True
+
+    return False
