08fe82770cefeb8e76b08b5d39bcc2fa52b1300e
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-11-20 11:13:39 -0300

    feat: Enviando requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    even_key = encrypt_message("ABBCCA", 2)
+    odd_key = encrypt_message("AABBCC", 1)
+
+    invalid_key = encrypt_message("AABBCC", -1)
+
+    assert even_key == "ACCB_BA"
+    assert odd_key == "A_CCBBA"
+
+    assert invalid_key == "CCBBAA"
+
+    with pytest.raises(TypeError) as error:
+        encrypt_message(98, 2)
+
+    assert str(error.value) == "tipo inválido para message"
